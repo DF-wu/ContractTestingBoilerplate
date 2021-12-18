d515e5e7f10dedf18b3e2167fc923131ec91f6ba
DF <a2470abc@gmail.com>
2021-12-18 17:22:56 +0800

    add providerType = ProviderType.ASYNCH

--- age-consumer/src/test/java/com/example/ageconsumer/pact/PactAgeConsumerTest.java
+++ age-consumer/src/test/java/com/example/ageconsumer/pact/PactAgeConsumerTest.java
@@ -5,6 +5,7 @@ import au.com.dius.pact.consumer.dsl.LambdaDsl;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.consumer.junit5.ProviderType;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.jayway.jsonpath.JsonPath;
@@ -20,7 +21,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(PactConsumerTestExt.class)
-@PactTestFor(providerName = "dateProvider", port = "1234")
+@PactTestFor(providerName = "dateProvider", port = "1234", providerType = ProviderType.ASYNCH)
 public class PactAgeConsumerTest {
 
     @Pact(consumer = "ageConsumer")
