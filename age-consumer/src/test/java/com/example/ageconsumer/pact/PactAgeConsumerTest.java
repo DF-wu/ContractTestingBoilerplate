3f2b2a0d4a7e8a6b805a12541e428e141a9848c3
DF <a2470abc@gmail.com>
2021-12-18 17:19:32 +0800

    change import source from ""

--- age-consumer/src/test/java/com/example/ageconsumer/pact/PactAgeConsumerTest.java
+++ age-consumer/src/test/java/com/example/ageconsumer/pact/PactAgeConsumerTest.java
@@ -1,13 +1,13 @@
 package com.example.ageconsumer.pact;
 
 import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.LambdaDsl;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.jayway.jsonpath.JsonPath;
-import io.pactfoundation.consumer.dsl.LambdaDsl;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.junit.jupiter.api.Test;
